fa6499fa809daf59108a5cd0f817d589acdacb21
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-26 11:25:24 -0800

    created new list page and added link to it on user index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,13 @@
 class ListsController < ApplicationController
   before_action :authenticate_user!, :except => [:main]
 
-
   def main
     if user_signed_in?
       redirect_to '/users/index'
     end
   end
-  
+
+  def new
+  end
+
 end
