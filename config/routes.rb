c48a46d39074ef794f0d75a7600d26dd4a0f1544
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-28 09:58:43 -0800

    fixed bug in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'lists#main'
   resources :lists
-  get 'users/:id', to: 'users#show'
   devise_for :users, :controllers => { registrations: 'registrations' }
+  get 'users/:id', to: 'users#show'
 end
